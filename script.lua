9655fe2143e098501518c7ea8a7e7b5463b01481
Zeerox <68605849+RunDTM@users.noreply.github.com>
2022-06-11 14:07:25 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
--- update
+-- settings fix update
 local a,    b,    c,    d,    e,    f,    g,    h,    j,    i,    j,    k,    l,    m,    n,    o,    p,    q,    r,    s,    t,    u,    v,    w,    x,    y,    z;                                   
 
 
